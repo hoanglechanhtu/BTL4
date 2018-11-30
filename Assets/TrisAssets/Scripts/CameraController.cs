ebe8f42096cb6f3575b9a430b9a6893bb683d17c
DESKTOP-FCTLT5M\hoang <hoanglechanhtu01@gmail.com>
2018-11-30 18:06:59 +0700

    Add camera controll button

---
+++ Assets/TrisAssets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float delta=5;
+    public Transform togo;
+    private Vector3 direction;
+	// Use this for initialization
+	void Start () {
+        direction = togo.position - transform.position;
+        direction.Normalize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void goUp() {
+        Vector3 position = transform.position;
+        position -= direction*delta;
+        transform.position = position;
+    }
+
+    public void goDown() {
+        Vector3 position = transform.position;
+        position += direction * delta;
+        transform.position = position;
+    }
+
+
+}
